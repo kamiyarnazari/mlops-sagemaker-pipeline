1e81af51f36aca90f8fa9f2c7ab622f60a95d4b1
Kamyar Nazari <kamiyarnazari@gmail.com>
2025-06-09 20:59:29 +0200

    Initial commit - CDK MLOps Infrastructure

--- mlops_cdk/mlops_cdk_stack.py
+++ mlops_cdk/mlops_cdk_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    RemovalPolicy,
+    aws_s3 as s3
 )
 from constructs import Construct
 
@@ -10,10 +11,6 @@ class MlopsCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MlopsCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        s3.Bucket(self, "RawDataBucket",
+                  versioned=True,
+                  removal_policy=RemovalPolicy.DESTROY)
